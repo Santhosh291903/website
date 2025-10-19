39798e8f3684bc884771853eef0ba85aa7bf0f81
kishor santhosh <santhoshprabhakaran03@gmail.com>
2025-10-19 14:59:29 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/website",
   server: {
     port: 4000, // Change the dev server port here
     open: true, // Optional: automatically open in browser
